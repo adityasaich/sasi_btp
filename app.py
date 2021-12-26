bb6bf9d8729235edc1f1f8ce4214c030f254e216
adityasaich <aditya66771@gmail.com>
2021-12-26 21:23:04 +0530

    setting port

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template,request
 import model
+import os
 app = Flask(__name__)
 classParams = {}
 import json
-
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 @app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
